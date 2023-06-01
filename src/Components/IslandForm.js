77e15f960ed858bc4289b376b9159011311756e0
unknown <almusallam296@hotmail.com>
2023-06-01 09:36:54 -0700

    Done

--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -1,12 +1,32 @@
+import { useState } from "react";
+
 export default function IslandForm({ island }) {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handelName = (e) => {
+    setName(e.target.value);
+  };
+  const handelPhone = (e) => {
+    setPhone(e.target.value);
+  };
+  const handelSubmit = () => {
+    window.confirm(
+      `Are you sure you want to book to ${island.name} with the Name: ${name}, phone: ${phone}`
+    );
+  };
   return (
     <div className="form">
       <h2>{island.name}</h2>
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
-      <input placeholder="Type Full Name" />
-      <input placeholder="Type Phone Number" />
-      <button className="book" onClick={() => {}}>
+      <input placeholder="Type Full Name" onChange={handelName} />
+      <input
+        placeholder="Type Phone Number"
+        type="tel"
+        onChange={handelPhone}
+      />
+      <button className="book" onClick={handelSubmit}>
         Book for today!
       </button>
     </div>
